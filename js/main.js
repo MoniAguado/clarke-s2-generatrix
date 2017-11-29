e0056f0ff08f04050ecc9d8b2557fb333768ebb4
gala1234 <calero.vg@gmail.com>
2017-11-29 17:09:49 +0100

    trying to resolve the issue

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
+//función para desplegar sección
 
+function abrir(idContent){
 
-//Función para menu diseño
-function choose(design){
-	document.getElementById(design).style.display = 'block';
+document.getElementById(idContent).style.display = 'block';
 }
 
-//función para seleccionar fecha de fin en la experiencia laboral
-
-// function hasta(endDate){
-// 	document.getElementById(endDate).styles;
-// }
-
-
-// -------------------------------------
-// //función para desplegar sección
-// function editar(idContent){
-// document.getElementById(idContent).style.display = 'block';
-// }
-//
-// //función para cerrar sección
-// function cerrar(idContent){
-//     document.getElementById(idContent).style.display = 'none';
-// }
+//función para cerrar sección
+function cerrar(idContent){
+	document.getElementById(idContent).style.display = 'none';
+}
