a05d8e18f89bafbb8000bd83271b50892bb695cd
Adalaber <maguado85@gmail.com>
2017-12-04 17:49:00 +0100

    upload image problems

--- js/main.js
+++ js/main.js
@@ -40,10 +40,8 @@ function completar () {
 	document.querySelector("#direccionPrompt").innerHTML = addressPrompt;
 }
 
-
-//subir imagen
 function archivo(evt) {
-      var files = evt.target.files; // FileList object
+      var files = evt.target.file; // FileList object
         // imagen del campo "file".
       for (var i = 0, f; f = files[i]; i++) {
            //Solo admitimos imágenes.
@@ -61,30 +59,8 @@ function archivo(evt) {
            reader.readAsDataURL(f);
        }
 
+			 var trigger = document.querySelector('.trigger');
+
 			 function triggerFile() {
-		   document.getElementById('#'+fileID).click();
+				trigger.addEventListener('click', '#'+fileID, trigger);
 		 }
-
-
-			//  function archivo2(evt) {
-			//  			      var files = evt.target.files; // FileList object
-			 //
-			//  			        //Obtenemos la imagen del campo "file".
-			//  			      for (var i = 0, f; f = files[i]; i++) {
-			//  			           //Solo admitimos imágenes.
-			//  			           if (!f.type.match('image.*')) {
-			//  			                continue;
-			//  			           }
-			 //
-			//  			           var reader = new FileReader();
-			 //
-			//  			           reader.onload = (function(theFile) {
-			//  			               return function(e) {
-			//  			               // Creamos la imagen.
-			//  			                      document.getElementById("prefoto").innerHTML = ['<img class="thumb2" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-			//  			               };
-			//  			           })(f);
-			 //
-			//  			           reader.readAsDataURL(f);
-			//  			       }
-			//  			}
