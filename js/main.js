7a0e561ba61821030377fe842551557cc23cec63
Adalaber <maguado85@gmail.com>
2017-12-04 12:14:30 +0100

    up files

--- js/main.js
+++ js/main.js
@@ -51,13 +51,40 @@ function archivo(evt) {
                 continue;
            }
 				 }
-      //      var reader = new FileReader();
-      //      reader.onload = (function(theFile) {
-      //          return function(e) {
-      //          // Creamos la imagen.
-      //                 document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-      //          };
-      //      })(f);
-      //      reader.readAsDataURL(f);
-      //  }
-}
+           var reader = new FileReader();
+           reader.onload = (function(theFile) {
+               return function(e) {
+               // Creamos la imagen.
+                      document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+               };
+           })(f);
+           reader.readAsDataURL(f);
+       }
+
+			 function triggerFile() {
+		   document.getElementById('#'+fileID).click();
+		 }
+
+
+			//  function archivo2(evt) {
+			//  			      var files = evt.target.files; // FileList object
+			 //
+			//  			        //Obtenemos la imagen del campo "file".
+			//  			      for (var i = 0, f; f = files[i]; i++) {
+			//  			           //Solo admitimos imágenes.
+			//  			           if (!f.type.match('image.*')) {
+			//  			                continue;
+			//  			           }
+			 //
+			//  			           var reader = new FileReader();
+			 //
+			//  			           reader.onload = (function(theFile) {
+			//  			               return function(e) {
+			//  			               // Creamos la imagen.
+			//  			                      document.getElementById("prefoto").innerHTML = ['<img class="thumb2" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+			//  			               };
+			//  			           })(f);
+			 //
+			//  			           reader.readAsDataURL(f);
+			//  			       }
+			//  			}
